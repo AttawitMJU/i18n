5cfde3a7cfa993a2c20b977e3788efc08b617065
attawit <attawit.mju@gmail.com>
2020-03-09 15:23:08 +0700

    master

---
+++ layouts/default.vue
@@ -0,0 +1,97 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="localePath(item.to)"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <!-- <v-list-item-title v-text="item.title" /> -->
+            <v-list-item-title v-text="link_menu[i]" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-btn icon @click.stop="miniVariant = !miniVariant">
+        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      </v-btn>
+      <v-btn icon @click.stop="clipped = !clipped">
+        <v-icon>mdi-application</v-icon>
+      </v-btn>
+      <v-btn icon @click.stop="fixed = !fixed">
+        <v-icon>mdi-minus</v-icon>
+      </v-btn>
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <nuxt-link v-if="$i18n.locale !== 'en'" :to="switchLocalePath('en')">
+        English
+      </nuxt-link>
+
+      <nuxt-link v-if="$i18n.locale !== 'th'" :to="switchLocalePath('th')">
+        ไทย
+      </nuxt-link>
+
+      <v-spacer />
+      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
+      <v-list>
+        <v-list-item @click.native="right = !right">
+          <v-list-item-action>
+            <v-icon light>
+              mdi-repeat
+            </v-icon>
+          </v-list-item-action>
+          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: require('@/assets/route/pages.json'),
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  },
+  computed: {
+    link_menu() {
+      return this.$t('link')
+    }
+  }
+}
+</script>
---
+++ pages/inspire.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-layout>
+    <v-flex class="text-center">
+      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+      <!-- <nuxt-link  :to="switchLocalePath('en')">
+        English
+      </nuxt-link>
+
+      <nuxt-link  :to="switchLocalePath('th')">
+        ไทย
+      </nuxt-link> -->
+      <blockquote class="blockquote">
+        &#8220;{{ $t('quote') }}&#8221;
+        <footer>
+          <small>
+            <em>&mdash;John Johnson</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-flex>
+  </v-layout>
+</template>
